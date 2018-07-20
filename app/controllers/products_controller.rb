7ef6a943aabb92587ce72bb1383e69741523bcb5
Emily Dong <emilyhfdong@gmail.com>
2018-07-20 19:34:31 +0000

    ratings

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,13 @@
 class ProductsController < ApplicationController
 
   def index
-    @products = Product.all.order(created_at: :desc)
+    @products = Product.all.order(created_at: :desc).includes(:reviews)
   end
 
   def show
     @product = Product.find params[:id]
     @reviews = @product.reviews.includes(:user)
+    @rating = (@reviews.average(:rating)).round(2)
     @logged_in = logged_in?
     @current_user = current_user
   end

55607c18ea7eca7c9edfbe141c2bc353dcd69f9d
Emily Dong <emilyhfdong@gmail.com>
2018-07-20 18:30:48 +0000

    submit review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,22 @@
 class ReviewsController < ApplicationController
   def create
-    raise "YAY IM HERE"
+    @product = Product.find(params[:product_id].to_i)
+    @review = @product.reviews.create(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to product_path(@product), notice: 'Review created!'
+    else
+      redirect_to product_path(@product), notice: 'Could not create review'
+    end
+
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+    )
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :password_digest, presence: true
 
 end

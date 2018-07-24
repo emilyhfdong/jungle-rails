576fdfe322cc78021908a99755ce012be185308f
Emily Dong <emilyhfdong@gmail.com>
2018-07-24 16:19:09 +0000

    products page featuer testing

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js:true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+
+  end
+  scenario "They can click on the header and navigate to the product page" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+
+    page.find('article').find('header').click
+    expect(page).to have_content('Reviews')
+    save_screenshot
+  end
+
+  scenario "They can click on the details button to navigate to the product page" do
+    visit root_path
+
+    page.find('article').find('.pull-right').click
+    expect(page).to have_content('Reviews')
+    save_screenshot
+  end
+end
+
+
